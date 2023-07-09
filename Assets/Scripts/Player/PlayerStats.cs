a7e4af2ebd3205516a1c457cad6195909cb8b81a
Martin Gerasimovic <mg243754@falmouth.ac.uk>
2023-07-09 16:55:34 +0100

    Gold and currency UI code

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,6 +1,9 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.UI;
+using TMPro;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -8,7 +11,9 @@ public class PlayerStats : MonoBehaviour
 
     public int p_Score;
     public int currency;
-
+    public int gold;
+    public TextMeshProUGUI currencyUI;
+    public TextMeshProUGUI goldUI;
 
     private void Start()
     {
@@ -20,6 +25,9 @@ public class PlayerStats : MonoBehaviour
         {
             Debug.Log("U lost");
         }
+        currencyUI.text = currency.ToString();
+        goldUI.text = gold.ToString();
+         
     }
 
     public void currencyAdd(int amount)
@@ -29,5 +37,13 @@ public class PlayerStats : MonoBehaviour
     public void currencyRemove(int amount)
     {
         currency -= amount;
+    }
+    public void goldAdd(int amount)
+    {
+        gold += amount;
+    }
+    public void goldRemove(int amount)
+    {
+        gold -= amount;
     }
 }
